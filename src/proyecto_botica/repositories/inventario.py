b19459f0339feb5a20f70755a833626179b6fdbb
Sandri-123 <sandro.choquec@istpargentina.edu.pe>
2025-12-03 15:14:01 -0500

    Iniciando inventario para importar

---
+++ src/proyecto_botica/repositories/inventario.py
@@ -0,0 +1,8 @@
+"""
+Modulo de Repositorio - Inventario
+Define las interfaces y servicios de acceso a datos
+"""
+
+from abc import ABC, abstractmethod
+from typing import List, Optional, Dict
+from ..models.producto import Producto, Categoria
