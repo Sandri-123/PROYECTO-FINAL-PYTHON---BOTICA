16ca0452623d7a92484cee26f3bec29179691afb
Sandri-123 <sandro.choquec@istpargentina.edu.pe>
2025-12-03 15:25:36 -0500

    Actualizando Inventario sgfunda parte

--- src/proyecto_botica/repositories/inventario.py
+++ src/proyecto_botica/repositories/inventario.py
@@ -6,3 +6,26 @@ Define las interfaces y servicios de acceso a datos
 from abc import ABC, abstractmethod
 from typing import List, Optional, Dict
 from ..models.producto import Producto, Categoria
+
+class IRepositorio(ABC):
+    """Interfaz para el repositorio de productos (Abstracción)."""
+
+    @abstractmethod
+    def agregar(self, producto: Producto) -> bool:
+        """Agrega un producto."""
+        pass
+
+    @abstractmethod
+    def obtener(self, id_producto: int) -> Optional[Producto]:
+        """Obtiene un producto por ID."""
+        pass
+
+    @abstractmethod
+    def obtener_todos(self) -> List[Producto]:
+        """Obtiene todos los productos."""
+        pass
+    
+    @abstractmethod
+    def eliminar(self, id_producto: int) -> bool:
+        """Elimina un producto."""
+        pass
